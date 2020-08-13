e40647dd3ece54ed8df5e3f1819dc8cadc0d88fe
Vinicius Faria de Souza <vinicius.faria@cvortex.io>
2020-08-13 20:21:56 -0300

    commit inicial

---
+++ src/main/java/br/com/vfs/api/payment/service/ApiPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.vfs.api.payment.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vfs/api/payment/service/ApiPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vfs.api.payment.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
